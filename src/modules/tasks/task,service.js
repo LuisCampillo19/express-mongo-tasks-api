296d4d2543fada5dd4177e91706768a9374a4404
LuisCampillo19 <lgcampillo19@gmail.com>
2026-02-07 15:20:27 -0500

    feat (services): add business logic for task completion

---
+++ src/modules/tasks/task,service.js
@@ -0,0 +1,22 @@
+import Task from './task.model.js';
+
+export const getAllTasks = async () => {
+    return await Task.find();
+};
+
+export const getTaskById = async (id) => {
+    return await Task.findById(id);
+};
+
+export const createTask = async () =>{
+    const newTask = new Task(taskData);
+    return await newTask.save();
+};
+
+export const updateTask = async (id, taskData) => {
+    return await Task.findByIdAndUpdate(id, taskData, { new: true });
+};
+
+export const deleteTask = async (id) => {
+    return await Task.findOneAndDelete(id);
+};
