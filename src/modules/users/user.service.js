59f22d6c4d1a2cdb2469985d55e0221f882ed130
LuisCampillo19 <lgcampillo19@gmail.com>
2026-02-07 17:55:56 -0500

    feat: add function controllers and model user

---
+++ src/modules/users/user.service.js
@@ -0,0 +1,12 @@
+import { Activity } from 'react';
+import User from './user.model.js';
+
+export const getAllUsers = async (req, res) =>{
+    return await User.find().select('-password');
+};
+
+
+export const createUser = async (req, res) =>{
+    const newUser = new User(userData); 
+    return await newUser.save();
+};
