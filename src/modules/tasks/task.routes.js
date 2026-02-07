72f8a3f4efffd73e85eb7c62fe8c310cfd821bba
LuisCampillo19 <lgcampillo19@gmail.com>
2026-02-07 17:38:23 -0500

    feat(routes): define endpoints for task management

---
+++ src/modules/tasks/task.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as taskController from './task.controller.js';
+
+const router = express.Router();
+
+router.get('/', taskController.getTasks);
+router.get('/:id', taskController.getTasks);
+router.post('/', taskController.create);
+router.put('/:id', taskController.update);
+router.delete('/:id', taskController.remove);
+
+export default router;
