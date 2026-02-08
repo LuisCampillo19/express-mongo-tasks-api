62ddfb7293122a4c1def89578a4d27adf25ba1b1
LuisCampillo19 <lgcampillo19@gmail.com>
2026-02-08 12:37:22 -0500

    feature (controllers): add function user controller

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -1,3 +1,20 @@
 import * as userService from './user.service.js';
 
-expor const 
+export const getUsers = async (req, res) => {
+    try {
+        const users = await userService.getAllUsers();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message});
+    }
+};
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const newUser = await userService.createUser({ username, email, password });
+        res.status(201).json({ message: 'Usuario registrado', user: newUser });
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+};
