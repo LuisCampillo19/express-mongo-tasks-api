59c509c5c0ceefd28ec17c2b54e8f083e645eebc
LuisCampillo19 <lgcampillo19@gmail.com>
2026-02-10 07:13:32 -0500

    change database name variable

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.BASE_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('DATABASE MONGO CONNECTED');
     } catch (err) {
         console.error('Error connecting to DB:', err);
